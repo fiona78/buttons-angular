6c8d828a11d568ec4c049bf83f04fd9ed18a1953
Fiona Przybylski <fiona@clearbooks.co.uk>
2014-07-17 16:58:13 +0100

    final draft

--- js/main-script.js
+++
@@ -1,9 +0,0 @@
-
- angular.module('radioExample', [])
-    .controller('ExampleController', ['$scope', function($scope) {
-      $scope.color = 'blue';
-      $scope.specialValue = {
-        "id": "12345",
-        "value": "green"
-      };
-}]);
